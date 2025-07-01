71a925b340a45c9aa2b645803ef101a2350004d9
pcyuen98 <pcyuen98@gmail.com>
2025-07-01 14:52:21 +0800

    Update BankApplication.java

--- bank/src/main/java/com/bank/BankApplication.java
+++ bank/src/main/java/com/bank/BankApplication.java
@@ -3,7 +3,7 @@ package com.bank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// remove message from the main..
+// remove message from the main..2
 @SpringBootApplication
 public class BankApplication {
 
