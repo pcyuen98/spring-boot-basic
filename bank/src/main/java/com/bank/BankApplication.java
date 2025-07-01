16f8ad87cb6d7fded65b02ef54bd07739f00c86b
pcyuen98 <36404290+pcyuen98@users.noreply.github.com>
2025-07-01 15:50:06 +0800

    Update BankApplication.java

--- bank/src/main/java/com/bank/BankApplication.java
+++ bank/src/main/java/com/bank/BankApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // update again from main..331
-//update again from branch..9
+//update again from branch..10
 
 @SpringBootApplication
 public class BankApplication {
