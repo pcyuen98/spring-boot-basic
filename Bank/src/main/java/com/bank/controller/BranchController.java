97f645a1c1cf1970ce4e723f12a27cb2bde63a92
pcyuen98 <pcyuen98@gmail.com>
2025-07-24 19:00:09 +0800

    updates

--- Bank/src/main/java/com/bank/controller/BranchController.java
+++ Bank/src/main/java/com/bank/controller/BranchController.java
@@ -11,29 +11,29 @@ package com.bank.controller;
 
 //b. BranchController with get by ID, get all, add, and delete by ID only
 
-//c. BranchDTO, BranchMapper and BranchMapperTest.java
+//c. BranchDTO, BranchMapper and BranchMapperTest.java 
+//Ensure table and column created on DB
 
 //d. BranchService and BranchServiceImpl
 
 //e. BranchRepo
 
-//f. Additional Requirement
-
-//g1. Exception Handling - When adding record, if the branchName is contain empty space, throw a DemoAppException with meaningful error message. i.e. Branch Name cannot be empty
+//Additional Requirement
+//f1. Exception Handling - When adding record, if the branchName is contain empty space, throw a DemoAppException with meaningful error message. i.e. Branch Name cannot be empty
 //Enable package scanning "com.demo.exceptions"
 //Ensure swagger output contains the DemoAppException type and error exist in the app.log file
 
-//g2. BranchRepo - Basic Search Function
+//f2. BranchRepo - Basic Search Function
 //Add a DOGET into controller above able to search by branchName
 //Add on the method to the controller, service and repo
 //Note: Refer to CustomerController.java getCustomersByDescriptionAndCreationDateBetween() as a sample
 
-//g3. BranchRepo - Search Function by date in between
+//f3. BranchRepo - Search Function by date in between
 //Add a DOGET into controller above able to between date from and date to
 //Add on the method to the controller, service and repo
 
 
-//h1 - UnitTesting - Create a BranchSearchTest.java for g2 and g3 above.
+//g1 - UnitTesting - Create a BranchSearchTest.java for g2 and g3 above.
 
 public class BranchController {
 
