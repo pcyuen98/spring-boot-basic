a8c77a61fe5726a943c3914d12909ce04d78edb3
pcyuen98 <pcyuen98@gmail.com>
2025-07-24 18:42:01 +0800

    updates

---
+++ Bank/src/main/java/com/bank/controller/BranchController.java
@@ -0,0 +1,40 @@
+package com.bank.controller;
+
+//Practical 8 - End to End Spring Boot 
+//Create a branch entity with the data below
+//a. BranchEntity.java
+
+//branchID - Index ID auto generated
+//branchName - length 100 - Not Nullable
+//branchPostCode - length 30 - Not Nullable
+//creationDate - Auto created when insert record - LocalDateTime 
+
+//b. BranchController with get by ID, get all, add, and delete by ID only
+
+//c. BranchDTO, BranchMapper and BranchMapperTest.java
+
+//d. BranchService and BranchServiceImpl
+
+//e. BranchRepo
+
+//f. Additional Requirement
+
+//g1. Exception Handling - When adding record, if the branchName is contain empty space, throw a DemoAppException with meaningful error message. i.e. Branch Name cannot be empty
+//Enable package scanning "com.demo.exceptions"
+//Ensure swagger output contains the DemoAppException type and error exist in the app.log file
+
+//g2. BranchRepo - Basic Search Function
+//Add a DOGET into controller above able to search by branchName
+//Add on the method to the controller, service and repo
+//Note: Refer to CustomerController.java getCustomersByDescriptionAndCreationDateBetween() as a sample
+
+//g3. BranchRepo - Search Function by date in between
+//Add a DOGET into controller above able to between date from and date to
+//Add on the method to the controller, service and repo
+
+
+//h1 - UnitTesting - Create a BranchSearchTest.java for g2 and g3 above.
+
+public class BranchController {
+
+}
