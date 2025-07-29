66296683349dbd96c2f74a980fdab294a801886e
pcyuen98 <pcyuen98@gmail.com>
2025-07-29 10:02:18 +0800

    updates

--- Bank/src/main/java/com/bank/entity/BranchEntity.java
+++ Bank/src/main/java/com/bank/entity/BranchEntity.java
@@ -4,7 +4,6 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
